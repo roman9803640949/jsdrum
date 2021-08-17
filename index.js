896a45ab0aea1b184bc2b13c39d19108f009691e
Roman Karki <roman.karki@sevadev.com>
2021-08-17 21:58:58 +0545

    added mouse events for click

--- index.js
+++ index.js
@@ -9,6 +9,27 @@ window.addEventListener('keydown', function (e) {
 })
 
 const keys = document.querySelectorAll('.key')
+
+keys.forEach(function (key) {
+    key.addEventListener('click', function (e) {
+        const attr = e.target.getAttribute('data-key')
+        const audio = document.querySelector(`audio[data-key="${attr}"]`)
+        if (!audio) return;
+        audio.currentTime = 0
+        audio.play();
+        this.classList.add('key__pressed')
+    })
+})
+function clickFunction (e) {
+    const audio = document.querySelector(`audio[data-key="${e.code}"]`)
+    const key = document.querySelector(`.key[data-key="${e.code}"]`)
+    if (!audio) return;
+    audio.currentTime = 0
+    audio.play();
+    key.classList.add('key__pressed')
+
+}
+
 keys.forEach(key => {
     key.addEventListener('transitionend', removeTransition)
 })
